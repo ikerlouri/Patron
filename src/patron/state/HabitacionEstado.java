f70a3761f459e3a4a649bf500cd3741969075bc4
Iker Lourido <plaiaundi@HZ305732.H012108.NET>
2024-03-05 13:24:34 +0100

    primer commit

---
+++ src/patron/state/HabitacionEstado.java
@@ -0,0 +1,11 @@
+package patron.state;
+
+public interface HabitacionEstado {
+	public HabitacionEstado ocupar();
+	public HabitacionEstado liberar();
+	public HabitacionEstado limpiar();
+	public HabitacionEstado salir();
+	
+ 
+ 
+}
